dcfc1c901fff990aaf3708a6009c8ebf757a8b10
nikelborm <r0075301net.r@yandex.ru>
2022-05-11 19:15:10 +0300

    asd

--- backend/scripts/ormgen.sh
+++ backend/scripts/ormgen.sh
@@ -1,12 +1,15 @@
-echo 'asdasd';
+echo -e 'schema:drop \n';
 
 node --require ts-node/register ./node_modules/typeorm/cli.js schema:drop -d src/config/data-source;
 
+echo -e 'migration:run \n';
 node --require ts-node/register ./node_modules/typeorm/cli.js migration:run -d src/config/data-source;
 
 read -p "Enter migation name: " migration_name;
 
-cat src/modules/infrastructure/model/user.model.ts
-cat src/modules/infrastructure/model/accessScope.model.ts
+echo -e 'migration:generate \n';
 node --require ts-node/register ./node_modules/typeorm/cli.js migration:generate -p src/modules/infrastructure/migration/$migration_name -d src/config/data-source;
 chown 1000:1000 src/modules/infrastructure/migration/*
+
+echo -e 'apply new migration \n';
+node --require ts-node/register ./node_modules/typeorm/cli.js migration:run -d src/config/data-source;
