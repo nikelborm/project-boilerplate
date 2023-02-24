9be30d05807620d6d2997bc151daeb0fd0d81f65
nikelborm <r0075301net.r@yandex.ru>
2023-02-24 05:59:46 +0300

    bnm

--- frontend/src/pages/Profile/Profile.tsx
+++ frontend/src/pages/Profile/Profile.tsx
@@ -1,8 +1,37 @@
-import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Manager } from 'socket.io-client';
+
+const manager = new Manager({
+  transports: ['websocket', 'polling'],
+  path: '/api/ws',
+  autoConnect: false,
+});
 
 export function Profile() {
-  const asd = useParams();
-  // eslint-disable-next-line no-console
-  console.log('Profile useParams: ', asd);
-  return <div>Profile</div>;
+  const socket = manager.socket('/test', {
+    auth: {
+      token: '123',
+    },
+  });
+
+  useEffect(() => {
+    socket.on('connect', () => {});
+
+    socket.on('disconnect', () => {});
+
+    socket.on('exception', (error) => {
+      // eslint-disable-next-line no-console
+      console.error('error: ', error);
+    });
+
+    socket.connect();
+    return () => {
+      // do not forget to off every new socket.on event handlers
+      socket.off('connect');
+      socket.off('disconnect');
+      socket.off('exception');
+    };
+  }, []);
+
+  return <div>Profile page</div>;
 }
