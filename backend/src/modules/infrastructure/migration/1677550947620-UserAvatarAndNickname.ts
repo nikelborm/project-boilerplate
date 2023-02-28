d5bb18cd343172db7ea58312f7c6c677b48bb847
nikelborm <r0075301net.r@yandex.ru>
2023-02-28 05:24:17 +0300

    asd

---
+++ backend/src/modules/infrastructure/migration/1677550947620-UserAvatarAndNickname.ts
@@ -0,0 +1,25 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class UserAvatarAndNickname1677550947620 implements MigrationInterface {
+  name = 'UserAvatarAndNickname1677550947620';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      ALTER TABLE "user"
+      ADD "nickname" character varying NOT NULL
+    `);
+    await queryRunner.query(`
+      ALTER TABLE "user"
+      ADD "avatar_url" character varying
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      ALTER TABLE "user" DROP COLUMN "avatar_url"
+    `);
+    await queryRunner.query(`
+      ALTER TABLE "user" DROP COLUMN "nickname"
+    `);
+  }
+}
