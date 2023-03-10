4f656b974ad06705ae77b1971d68842741466aaa
nikelborm <r0075301net.r@yandex.ru>
2023-03-10 05:04:09 +0300

    asd

--- backend/src/modules/accessScope/accessScope.controller.ts
+++ backend/src/modules/accessScope/accessScope.controller.ts
@@ -19,6 +19,6 @@ export class AccessScopeController {
     accessScope: UpdateAccessScopeDTO,
   ): Promise<EmptyResponseDTO> {
     await this.accessScopeUseCase.updateOne(accessScope);
-    return { response: {} };
+    return {};
   }
 }
